6e5296e5cc72a3d146aae627ac45e9453f74d693
Greg Lin <glin@glin.io>
2020-03-14 00:09:36 -0500

    Remove prop types from build

--- webpack.config.js
+++ webpack.config.js
@@ -3,61 +3,66 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
 
-module.exports = {
-  entry: path.join(__dirname, 'srcjs', 'index.js'),
+module.exports = (env, argv) => {
+  // Set NODE_ENV for Babel
+  process.env.NODE_ENV = argv.mode
 
-  output: {
-    filename: 'reactable.js',
-    path: path.join(__dirname, 'inst', 'htmlwidgets')
-  },
+  return {
+    entry: path.join(__dirname, 'srcjs', 'index.js'),
 
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        extractComments: false
-      }),
-      new OptimizeCssAssetsPlugin()
-    ]
-  },
+    output: {
+      filename: 'reactable.js',
+      path: path.join(__dirname, 'inst', 'htmlwidgets')
+    },
 
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'reactable.css'
-    })
-  ],
+    optimization: {
+      minimizer: [
+        new TerserPlugin({
+          extractComments: false
+        }),
+        new OptimizeCssAssetsPlugin()
+      ]
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: [require('autoprefixer')]
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'reactable.css'
+      })
+    ],
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: 'babel-loader'
+        },
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: [require('autoprefixer')]
+              }
             }
-          }
-        ]
-      }
-    ]
-  },
+          ]
+        }
+      ]
+    },
 
-  externals: {
-    react: 'window.React',
-    'react-dom': 'window.ReactDOM',
-    reactR: 'window.reactR'
-  },
+    externals: {
+      react: 'window.React',
+      'react-dom': 'window.ReactDOM',
+      reactR: 'window.reactR'
+    },
 
-  stats: {
-    colors: true
-  },
+    stats: {
+      colors: true
+    },
 
-  devtool: 'source-map'
+    devtool: 'source-map'
+  }
 }
