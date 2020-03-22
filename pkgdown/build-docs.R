ad234fbfa5582a8687f1fb034eaecd103e6b640c
Greg Lin <glin@glin.io>
2020-03-22 14:49:10 -0500

    Update doc build script for stylesheet extraction

--- pkgdown/build-docs.R
+++ pkgdown/build-docs.R
@@ -7,7 +7,7 @@ example <- xml2::xml_find_first(index_html, ".//*/comment()[contains(., 'pkgdown
 rmarkdown::render("pkgdown/example.Rmd", output_dir = "docs")
 example_html <- xml2::read_html("docs/example.html")
 widget_html <- xml2::xml_find_first(example_html, ".//*[contains(@class, 'reactable')]")
-widget_deps <- xml2::xml_find_all(example_html, ".//script")
+widget_deps <- xml2::xml_find_all(example_html, ".//script|.//link")
 
 for (dep in rev(widget_deps)) {
   xml2::xml_add_sibling(example, dep)
