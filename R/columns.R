e76d22026afac492b009fb4c9ed8febb5c0f6279
Greg Lin <glin@glin.io>
2021-08-22 15:56:11 -0500

    Make links in documentation more descriptive

--- R/columns.R
+++ R/columns.R
@@ -373,8 +373,8 @@ is.colGroup <- function(x) {
 #'   a locale is unsupported. When multiple locales are specified, the first
 #'   supported locale will be used.
 #'
-#'   See [here](https://docs.microsoft.com/en-us/openspecs/office_standards/ms-oe376/6c085406-a698-4e12-9d4d-c3b0ee3dbc4a)
-#'   for a list of common BCP 47 language tags.
+#'   See a list of [common BCP 47 language tags](https://docs.microsoft.com/en-us/openspecs/office_standards/ms-oe376/6c085406-a698-4e12-9d4d-c3b0ee3dbc4a)
+#'   for reference.
 #' @return A column format object that can be used to customize data formatting
 #'   in `colDef()`.
 #'
